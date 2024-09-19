0d86c3fd5cabc28c8e7185ec9cbd7885556329cc
toila4120 <toila4120@gmail.com>
2024-09-20 01:06:46 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart'; // Add this line
-import 'package:its_tech_app/locale/app_localizations.dart';
-import 'package:its_tech_app/locale/languages.dart';
-import 'package:its_tech_app/screen/machine_total/machine_total_screen.dart';
+import 'package:its_tech_app/screen/main_screen/screen/main_screen.dart';
 
-import 'locale/language_vi.dart';
-
-BaseLanguage language = LanguageVi();
-
-void main() async {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      locale: const Locale('vi'),
-      localizationsDelegates: [
-        AppLocalizations(),
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('en'), // Define supported locales
-        Locale('vi'), // Assuming you also want to support Vietnamese
-      ],
-      home: Scaffold(
-        body: MachineTotalScreen(),
-      ),
-    ),
+MaterialApp main() {
+  return const MaterialApp(
+    home: MyApp(),
   );
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: SafeArea(child: MainScreen(),),);
+  }
+}
+
